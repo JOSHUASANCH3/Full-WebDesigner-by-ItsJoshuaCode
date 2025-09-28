2e31856d2ad101849a610541721eaeb628247686
JOSHUASANCH3 <jokayou3@gmail.com>
2025-09-28 17:25:44 -0600

    🗓 Day #15: Completado la seccion de new Arrivals.
    📦 feat: Se agrego #newArrivals como seccion el pagina.
    
    🛠️ fix: Se corrigieron clases y estructua del html en la seccion.
    
    ✨ style: Se completo el diseño de la seccion newArrivals.
    
    📝 docs : Actualización de documentación.
    
    🔧 refactor : Ninguno.
    
    🚀 perf: Ninguno.

--- week-2/Ecommerce-html-css-js/assets/config/main.js
+++ week-2/Ecommerce-html-css-js/assets/config/main.js
@@ -38,6 +38,33 @@ const swiperCategories = new Swiper(".categories__container", {
 });
 
 /*=============== SWIPER PRODUCTS ===============*/
+const swiperProducts = new Swiper(".container__new-arrivals", {
+    spaceBetween: 24,
+    loop: true,
+    // Parameters Navigations
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    // Responsive breakpoints
+    breakpoints: {
+        // when window width is >= 640px
+        640: {
+            slidesPerView: 2,
+            spaceBetween: 24,
+        },
+        // when window width is >= 768px
+        768: {
+            slidesPerView: 4,
+            spaceBetween: 24,
+        },
+        // when window width is >= 1400px
+        1400: {
+            slidesPerView: 4,
+            spaceBetween: 24,
+        },
+    },
+});
 
 /*=============== PRODUCTS TABS ===============*/
 const tabs = document.querySelectorAll("[data-target]"),
